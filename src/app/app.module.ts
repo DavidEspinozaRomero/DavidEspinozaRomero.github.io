356db2188ce5029e560aa394580b0466bf6d9e24
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-04-16 15:01:40 -0500

    aoeu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Components } from './components/components.module';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
