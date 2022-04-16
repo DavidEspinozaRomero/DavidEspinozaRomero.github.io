5d1b212030ace1f4b46b517e9cb24bc624c542c3
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-04-16 14:58:08 -0500

    module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SidebartestComponent } from "./sidebartest/sidebartest.component";
 
 @NgModule({
   declarations: [SidebartestComponent],
   exports: [SidebartestComponent],
-  imports: [],
+  imports: [ CommonModule ],
   providers: [],
 })
 export class ComponentsModule {}
