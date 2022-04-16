1b82dc3372f25b9e6e5d002c7db0b90f0227db34
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-04-16 14:43:23 -0500

    test

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { SidebartestComponent } from "./sidebartest/sidebartest.component";
+
+@NgModule({
+  declarations: [SidebartestComponent],
+  imports: [],
+  providers: [],
+})
+export class Components {}
---
+++ src/app/components/sidebartest/sidebartest.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-sidebartest',
+    templateUrl: './sidebartest.component.html',
+    styleUrls: ['./sidebartest.component.scss']
+})
+export class SidebartestComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
