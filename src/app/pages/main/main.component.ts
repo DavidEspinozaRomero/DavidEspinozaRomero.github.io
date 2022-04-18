a2fe16ca39dfb1e186ec4ccef5baaf72ca1baf9a
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-04-17 19:06:25 -0500

    class="card-img ar-4"

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./main.component.scss"],
 })
 export class MainComponent implements OnInit {
+  s3Items = [
+    {
+      number: 01,
+      title: "Find",
+      description:
+        "We will find clients in your area looking for your business services through our targeted marketing systems.",
+    },
+    {
+      number: 02,
+      title: "Connect",
+      description:
+        "We will connect them real time to your business through multiple means.",
+    },
+    {
+      number: 03,
+      title: "Grow",
+      description:
+        "We will grow your business by supporting your ever expanding customer base and helping you manage your business growth.",
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
