f37f49ff6591fab72b98e41484b142684bea5aaa
David Espinoza <deerhou@gmail.com>
2022-04-18 10:17:08 -0500

    agregando mejoras a page not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { PagesModule } from './pages/pages.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { PagesModule } from './pages/pages.module';
     BrowserModule,
     ComponentsModule,
     PagesModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
