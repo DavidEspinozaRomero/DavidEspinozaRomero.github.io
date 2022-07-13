927b33bbca234306bebaad84c1d62e95c948b3f5
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-07-12 21:12:45 -0500

    creating pages

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { PagesRoutingModule } from './pages.routing';
-import { MainComponent } from './main/main.component';
-import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
+import { PagesRoutingModule } from "./pages.routing";
+import { MainComponent } from "./main/main.component";
+import { PathNotFoundComponent } from "./path-not-found/path-not-found.component";
+import { ContactComponent } from "./contact/contact.component";
+import { AboutUsComponent } from "./about-us/about-us.component";
+import { ProjectsComponent } from "./projects/projects.component";
 
 @NgModule({
-  declarations: [MainComponent, PathNotFoundComponent],
-  exports: [MainComponent, PathNotFoundComponent],
+  declarations: [
+    MainComponent,
+    ContactComponent,
+    AboutUsComponent,
+    ProjectsComponent,
+    PathNotFoundComponent,
+  ],
+  exports: [],
   imports: [CommonModule, PagesRoutingModule],
   providers: [],
 })
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -2,13 +2,19 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { MainComponent } from './main/main.component';
+import { ProjectsComponent } from "./projects/projects.component";
+import { AboutUsComponent } from "./about-us/about-us.component";
+import { ContactComponent } from "./contact/contact.component";
 
 
 const routes: Routes = [
   
   { path: "", component: MainComponent },
-  // { path: "404", component: PathNotFoundComponent },
-  // { path: "**", pathMatch: "full", redirectTo: "404" },
+  { path: "projects", component: ProjectsComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "about", component: AboutUsComponent },
+  { path: "404", component: PathNotFoundComponent },
+  { path: "**", pathMatch: "full", redirectTo: "404" },
 ];
 
 @NgModule({
