fa1ec88e6c5d8fa6b08d60f2d0969cbf6b6430e3
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-07-12 21:25:53 -0500

    router

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
-import { CommonModule, RouterModule } from "@angular/common";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 import { MenuComponent } from "./menu/menu.component";
 
 @NgModule({
