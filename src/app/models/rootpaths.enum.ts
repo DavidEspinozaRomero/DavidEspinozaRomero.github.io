7f86a05277ddbbd8311a1ec95316b2cf9db0c79f
david espinoza <ivieduca.david.e@gmail.com>
2023-02-14 17:28:05 -0500

    Auto-generated commit [ci skip]

--- src/app/models/rootpaths.enum.ts
+++
@@ -1,6 +0,0 @@
-export const enum RoutingRootPaths {
-  home = '',
-  contact = 'contact',
-  projects = 'projects',
-  aboutUs = 'about-us',
-}
--- src/app/shared/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './components/footer/footer.component';
-export * from './components/header/header.component';
